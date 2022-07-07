b76ade7db8917d2a6fc465c21f4a7c6b4bd80f73
Aleg <Aleg@DESKTOP-PQC9QK1>
2022-07-07 19:35:01 +0300

    Добавьте файлы проекта.

---
+++ 5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _5._1
+{
+    class Program
+    {
+        static string[] WordSplitter(string phrase)
+        {
+            string[] result = phrase.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return result;
+        }
+
+        static void PrintResult(string[] result)
+        {
+            foreach (var word in result)
+            {
+                Console.WriteLine($"{word}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинное предложение (используя пробелы):");
+            string phrase = Console.ReadLine();
+
+            string[] words = WordSplitter(phrase);
+
+            PrintResult(words);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ 5.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _5._2
+{
+    class Program
+    {
+
+        static string WordSplitter(string phrase)
+        {
+            string[] wordsSplit = phrase.Split(' ');
+            string Text = ReversWords(wordsSplit);
+            return Text;
+        }
+        static string ReversWords(string[] phrase)
+        {
+            Array.Reverse(phrase);
+            string result = string.Join(" ", phrase);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите длинное предложение (используя пробелы):");
+            string words = Console.ReadLine();
+            words = WordSplitter(words);
+
+            Console.WriteLine(words);
+
+            Console.ReadLine();
+        }
+    }
+}
